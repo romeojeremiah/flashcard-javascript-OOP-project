84de205adc857b65201b2f1f4e7da8b85aa7aabc
Romeo <romeojeremiah@gmail.com>
2019-10-14 14:09:08 -0500

    finished project with local storage

---
+++ flashcards/flashcards/flashcards-setup/js/app.js
@@ -0,0 +1,10 @@
+// const showBtn = document.getElementById("show-btn");
+// const questionCard = document.querySelector(".question-card");
+// const closeBtn = document.querySelector(".close-btn");
+// const form = document.getElementById("question-form");
+// const feedback = document.querySelector(".feedback");
+// const questionInput = document.getElementById("question-input");
+// const answerInput = document.getElementById("answer-input");
+// const questionList = document.getElementById("questions-list");
+// let data = [];
+// let id = 1;
